272f3e9cb07997e9c78567a750efcfe225ee71fc
Mustafa Derin <mustafa.derin@sabanciuniv.edu>
2025-11-01 10:42:47 +0300

    Add files via upload

---
+++ Frontend TeknoSU/src/main.jsx
@@ -0,0 +1,70 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import AuthProvider from './contexts/AuthProvider.jsx'
+import 'swiper/css';
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// fonts and icons
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
+import {
+createBrowserRouter,
+RouterProvider,
+} from "react-router-dom";
+import Home from './home/Home.jsx';
+import Blog from './blog/Blog.jsx';
+import Shop from './shop/Shop.jsx';
+import SingleProduct from './shop/SingleProduct.jsx';
+import CartPage from './shop/CartPage.jsx';
+import Login from './components/Login.jsx';
+import PrivateRoute from './PrivateRoute.jsx/PrivateRoute.jsx'
+import Signup from './components/Signup.jsx'
+
+const router = createBrowserRouter([
+{
+path: "/",
+element: <App/>,
+children: [
+  {path: "/",
+    element: <Home/>
+  },
+  {path: "/blog",
+    elemnt: <Blog/>
+  },
+  {
+    path: "/shop",
+    element: <Shop/>
+  },
+   {
+    path: "/shop/:id",
+    element: <SingleProduct/>
+   },
+   {
+    path: "/cart-page",
+    element: <PrivateRoute><CartPage/></PrivateRoute>
+   },
+   {
+    path: "/login",
+    element: <Login/>
+   },
+   {
+    path: "/sign-up",
+    element: <Signup/>
+   }
+]
+}
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <RouterProvider router= {router}/>
+  </AuthProvider>)
+
